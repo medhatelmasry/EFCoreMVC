0abfe3f4364ebbbfe28f31add7435afa4b43af4e
Medhat Elmasry <medhatelmasry@live.com>
2018-09-28 13:23:43 -0700

    initial commit

---
+++ Models/NW/Categories.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreMVC.Models.NW
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "You must enter the {0}.")]
+        [Display(Name = "Category")]
+        public string CategoryName { get; set; }
+        [Required]
+        //[DataType(DataType.EmailAddress)]
+
+        [StringLength(30, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 6)]
+        //[RegularExpression (pattern:"[a]{10}")]
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        public ICollection<Products> Products { get; set; }
+    }
+}
---
+++ Models/NW/Products.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreMVC.Models.NW
+{
+    public partial class Products
+    {
+        public Products()
+        {
+            OrderDetails = new HashSet<OrderDetails>();
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int? SupplierId { get; set; }
+        public int? CategoryId { get; set; }
+        public decimal? UnitPrice { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string QuantityPerUnit { get; set; }
+        [ScaffoldColumn(false)]
+        public short? UnitsInStock { get; set; }
+        [ScaffoldColumn(false)]
+        public short? UnitsOnOrder { get; set; }
+        [ScaffoldColumn(false)]
+        public short? ReorderLevel { get; set; }
+        [ScaffoldColumn(false)]
+        public bool? Discontinued { get; set; }
+
+        public Categories Category { get; set; }
+        public Suppliers Supplier { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
